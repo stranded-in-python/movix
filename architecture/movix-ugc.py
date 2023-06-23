b83d801df364254ea67b6f1e2df09d0d3655ffa7
sbkubric <sbkubric@gmail.com>
2023-06-23 21:07:09 +0300

    fix: improved to-be, ugc

--- architecture/movix-ugc.py
+++ architecture/movix-ugc.py
@@ -1,29 +1,28 @@
 from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.network import Endpoint
-from diagrams.c4 import Container, Database, Relationship, SystemBoundary
+from diagrams.c4 import Container
 from diagrams.onprem.database import ClickHouse
 from diagrams.onprem.network import Nginx
 from diagrams.onprem.queue import Kafka
 
-with Diagram("UGC Service", show=False, direction="TB"):
+with Diagram(
+    "UGC Service", show=False, direction="TB", graph_attr={"splines": "spline"}
+):
     gateway = Nginx("API Gateway")
     with Cluster("API"):
         endpoint = Endpoint("Endpoint")
-        with SystemBoundary("Authentication & Authorization"):
-            token_validator = Container(
-                "TokenValidator", description="Validate token signature"
-            )
-            access_checker = Container(
-                "AccessChecker", description="Retrieve rights list from DB and check"
-            )
-        payload_serializer = Container(
-            "PayloadSerializer", description="Validate and serialize data"
+        token_validator = Container(
+            "Token Validator", description="Validate token signature"
         )
+        payload_serializer = Container(
+            "Payload Serializer", description="Validate and serialize data"
+        )
+        api_kafka = Container("Kafka Producer")
 
     kafka = Kafka("UGC Storage")
 
     with Cluster("UGC ETL"):
-        etl_kafka = Container("KafkaConnector")
+        etl_kafka = Container("Kafka Consumer")
         data_transformer = Container(
             "Transformer", description="Prepare data before loading"
         )
@@ -33,8 +32,7 @@ with Diagram("UGC Service", show=False, direction="TB"):
 
     gateway >> Edge(label="Incoming packet") >> endpoint
 
-    access_checker >> token_validator
-    access_checker << kafka
-    endpoint >> access_checker >> payload_serializer >> kafka
+    endpoint >> token_validator
+    endpoint >> payload_serializer >> api_kafka >> kafka
 
     kafka >> etl_kafka >> data_transformer >> etl_olap >> olap
